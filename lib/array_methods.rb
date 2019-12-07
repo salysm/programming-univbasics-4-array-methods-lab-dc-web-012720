719b0cb2dff630635a10e2039fd80c39a64fc08e
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-07 23:13:14 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def using_include(array, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    array.includes?("wow")
-    array.includes?("Tardis")
+    array.include?("wow")
+    array.include?("Tardis")
 end
 
 def using_sort(array)
